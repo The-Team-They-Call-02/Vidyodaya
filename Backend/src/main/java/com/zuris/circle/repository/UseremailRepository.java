aa9f3344bfde100220d0f0533d965379ea9feb78
EmilyA <emilyadams.3d@gmail.com>
2020-11-16 15:47:36 -0800

    Renamed the backend project properly, it had lambda in the title

---
+++ Backend/src/main/java/com/zuris/circle/repository/UseremailRepository.java
@@ -0,0 +1,12 @@
+package com.zuris.circle.repository;
+
+import com.zuris.circle.models.Useremail;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * The CRUD Repository connecting Useremail to the rest of the application
+ */
+public interface UseremailRepository
+    extends CrudRepository<Useremail, Long>
+{
+}
