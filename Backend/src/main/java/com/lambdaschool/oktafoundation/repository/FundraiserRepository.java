a6065bc812fc905ab67a79bbdb55072ffa707f8c
EmilyA <emilyadams.3d@gmail.com>
2020-11-17 11:04:25 -0800

    Changed name of cover image

---
+++ Backend/src/main/java/com/lambdaschool/oktafoundation/repository/FundraiserRepository.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.oktafoundation.repository;
+
+import com.lambdaschool.oktafoundation.models.Fundraiser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface FundraiserRepository extends CrudRepository<Fundraiser, long>
+{
+
+}
