8417e71487ff307b93583a6f71f9898dae532ab0
EmilyA <emilyadams.3d@gmail.com>
2020-11-19 17:35:22 -0800

    Worked a bit on page styling

---
+++ frontend/src/Components/Blog.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { PageBody, Card } from '../Styles/PageStyle';
+
+const Blog = () => {
+
+    return(
+        <PageBody>
+        <h1>Blogs</h1>
+        <br/>
+        <Card>
+            <h2>Vidyodaya Children’s Expressions in the CONSTELLATION</h2>
+            <img src="" alt=""/>
+            <p>Expressions: Art from children expresses their world in their own perspectives and has never failed to amaze us.
+
+            CONSTELLATION is a France based initiative: They work on the theme ‘Art to help the world’s children grow and connect in the world’. Their goal is to encourage the human, economic and cultural development of children and teens through artistic creation.
+
+            In the recent edition of their gallery the expressions of our Vidyodaya children has been published.</p>
+        </Card>
+        </PageBody>
+    )
+}
+    
+export default Blog;
