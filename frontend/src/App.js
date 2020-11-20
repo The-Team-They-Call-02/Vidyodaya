46d5d2514660e3faf816604a66fb9ed87463a054
EmilyA <emilyadams.3d@gmail.com>
2020-11-19 16:17:08 -0800

    Forgot to add Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Programs from './Programs/Programs';
 import About from './Components/About';
 
 
 function App() {
   return (
-    <div >
+    <Router>
     test
+    {/* <Route exact path="/">
+      <Main />
+    </Route> */}
     <Route path="/about">
       <About/>
     </Route>
     <Route path="/programs">
     <Programs/>
     </Route>
-    </div>
+    </Router>
   );
 }
 
