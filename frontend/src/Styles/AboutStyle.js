ec3156d720a7c2e2d0610c2aba25be603cde3c63
EmilyA <emilyadams.3d@gmail.com>
2020-11-20 20:33:58 -0800

    More work on About page

--- frontend/src/Styles/AboutStyle.js
+++ frontend/src/Styles/AboutStyle.js
@@ -3,19 +3,22 @@ import { colorObj } from './GlobalStyles.js';
 
 export const Header = styled.h1`
 font-family: Arizonia, cursive;
-font-size: 72px;
-font-style: normal;
+font-size: 60px;
+font-style: normal;<br/
 font-weight: 400;
 line-height: 90px;
 letter-spacing: 0em;
 text-align: center;
+
 `;
 
-export const HistorySection = styled.div`
+export const StyledSection = styled.div`
 width: 100%;
 height: 100vh;
 display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
+padding-top: 40%;
+padding-bottom: 60%;
 `;
