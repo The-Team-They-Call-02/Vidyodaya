6e21811d811a6732c19f6c1cf4cd922ae59a5817
mpaolodr <mpaolodr_1504@yahoo.com>
2020-11-21 15:08:18 -0500

    reports home done

--- frontend/src/Components/pages/Reports/Reports.styles.jsx
+++ frontend/src/Components/pages/Reports/Reports.styles.jsx
@@ -1,9 +1,53 @@
 import styled from "styled-components";
+import { colorObj } from "../../../Styles/GlobalStyles";
 
 const MainContainer = styled.div`
-  border: 2px solid red;
   background: #f8f7f7;
   padding: 150px 0 100px 0;
+  position: relative;
 `;
 
-export { MainContainer };
+const SocialIconsContainer = styled.div`
+  width: 50px;
+  text-align: center;
+  position: absolute;
+  top: 250px;
+  right: 20px;
+`;
+
+const SocialIcons = styled.a`
+  text-decoration: none;
+  font-size: 4rem;
+  color: ${(props) => (props.color ? props.color : "")};
+`;
+
+const Container = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+  height: 100%;
+`;
+
+const HeadingContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  height: 150px;
+`;
+
+const Heading = styled.h2`
+  font-size: 6.4rem;
+  font-family: "Roboto", sans-serif;
+  font-weight: 700;
+  color: ${colorObj.dark};
+`;
+
+export {
+  MainContainer,
+  Container,
+  SocialIconsContainer,
+  SocialIcons,
+  HeadingContainer,
+  Heading,
+};
--- frontend/src/Components/pages/Reports/ReportsHome/ReportsHome.component.jsx
+++ frontend/src/Components/pages/Reports/ReportsHome/ReportsHome.component.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 
+// styled-components
+import { Container, HeadingContainer, Heading } from "../Reports.styles";
+
+import { LinksContainer, Links } from "./ReportsHome.styles";
+
 const ReportsHome = () => {
   return (
-    <div>
-      <h2>ReportsHome</h2>
-    </div>
+    <Container>
+      <HeadingContainer>
+        <Heading>Reports</Heading>
+      </HeadingContainer>
+
+      <LinksContainer>
+        <Links to="/reports/annual">Annual Reports</Links>
+        <Links to="/reports/financial">Financial Reports</Links>
+        <Links to="/reports/other">Other Reports</Links>
+      </LinksContainer>
+    </Container>
   );
 };
 
