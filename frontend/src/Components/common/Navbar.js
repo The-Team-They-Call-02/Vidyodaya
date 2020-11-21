0c5a5ec18e880f946dd817ec68fd3f7a2c255f70
EmilyA <emilyadams.3d@gmail.com>
2020-11-20 16:25:40 -0800

    Removed a warning from the navbar section

--- frontend/src/Components/common/Navbar.js
+++ frontend/src/Components/common/Navbar.js
@@ -9,7 +9,6 @@ const Navbar = () => {
         <NavbarStyle>
             <img src={logo} style={{height: "150px"}} alt=""/>
             <NavLink to="/"><NavText>Welcome</NavText></NavLink>
-            
             <NavLink to="/about"><NavText>About</NavText></NavLink>
             <NavLink to="/programs"><NavText>Programs</NavText></NavLink>
             <NavLink to="/get-involved"><NavText>Get Involved</NavText></NavLink>
@@ -17,8 +16,8 @@ const Navbar = () => {
             <NavLink to="/articles"><NavText>Articles</NavText></NavLink>
             <NavLink to="/contact"><NavText>Contact</NavText></NavLink>
             <NavLink to="/login"><NavText>Login</NavText></NavLink>
-            <a href="https://www.payumoney.com/paybypayumoney/#/9F4C7B2FCB7AA9833CAD8D3FDEC46DD7" target="_blank">
-            <button>Donate</button>
+            <a href="https://www.payumoney.com/paybypayumoney/#/9F4C7B2FCB7AA9833CAD8D3FDEC46DD7" target="_blank" rel="noreferrer">
+                <button>Donate</button>
             </a>
         </NavbarStyle>
     )
