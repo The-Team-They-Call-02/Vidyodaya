1e7ae068c1541dd541258f13b0dc15bde72dfb80
Harry Henry Gebel <harry@gebel.tech>
2020-11-22 08:31:39 -0500

    Add Camp model

---
+++ backend/src/main/java/com/opportunity/hack/vidyodaya/models/Camp.java
@@ -0,0 +1,58 @@
+package com.opportunity.hack.vidyodaya.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "camps")
+public class Camp {
+
+  /**
+   * The database id of the camp
+   */
+  @Id
+  @GeneratedValue
+  private long campId;
+
+  /**
+   * The camp's title
+   */
+  private String title;
+
+  /**
+   * The camp's subtitle
+   */
+  private String subtitle;
+
+  /**
+   * The description of the camp
+   */
+  private String description;
+
+  /**
+   * Number of participants
+   */
+  @NotNull
+  private long participants;
+
+  /**
+   * Number of facilitators
+   */
+  @NotNull
+  private long facilitators;
+
+  /**
+   * The number of villages covered by the camp
+   */
+  private long villagesCovered;
+
+  /**
+   * The highlights of the camp
+   */
+  @OneToMany(mappedBy = "camp", cascade = CascadeType.ALL, orphanRemoval = true)
+  @JsonIgnoreProperties(value = "camp", allowSetters = true)
+  private Set<Highlight> highlights = new HashSet<>();
+}
