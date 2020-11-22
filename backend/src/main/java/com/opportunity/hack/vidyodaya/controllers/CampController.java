cfeacf8c69f1a76db2b471918b87d821bac3eb8a
Harry Henry Gebel <harry@gebel.tech>
2020-11-22 09:52:22 -0500

    Create camp controller

---
+++ backend/src/main/java/com/opportunity/hack/vidyodaya/controllers/CampController.java
@@ -0,0 +1,8 @@
+package com.opportunity.hack.vidyodaya.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/camps")
+public class CampController {}
