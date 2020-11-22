93c9bea764fcdd1c5f6cc4827519d9b1e21c5664
ianbautista <ianfbautista@gmail.com>
2020-11-22 11:10:58 -0800

    builts programs page

--- frontend/src/Components/pages/Programs/Programs.component.jsx
+++ frontend/src/Components/pages/Programs/Programs.component.jsx
@@ -1,32 +1,48 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
-
 // components
 import ProgramsHome from "./ProgramsHome/ProgramsHome.component";
 import FrisbeeProgram from "./FrisbeeProgram/FrisbeeProgram.component";
-import ChildrenCamps from "./ChildrenCamps/ChildrenCamps.component"
+import ChildrenCamps from "./ChildrenCamps/ChildrenCamps.component";
 import EducationProgram from "./EducationProgram/EducationProgram.component";
 import Balwadis from "./Balwadis/Balwadis.component";
 import Culture from "./Culture/Culture.componet";
-
+import CampCard from "./ChildrenCamps/CampCard/CampCard.component";
 
 // styled-components
 import { MainContainer } from "./Programs.styles";
 
 const Programs = () => {
-  return (
-    <MainContainer>
-      <Switch>
-        <Route path="/programs/" component={ProgramsHome} />
-        <Route path="/programs/frisbee-program" component={FrisbeeProgram}/>
-        <Route path="/programs/children-camps" component={ChildrenCamps} />
-        <Route path="/programs/education-program" component={EducationProgram} />
-        <Route path="/programs/balwadis" component={Balwadis} />
-        <Route path="/programs/culture" component={Culture} />
-      </Switch>
-    </MainContainer>
-  );
+	return (
+		<MainContainer>
+			<Switch>
+				<Route
+					exact
+					path="/programs/children-camps/:id"
+					component={CampCard}
+				/>
+				<Route exact path="/programs/culture" component={Culture} />
+				<Route exact path="/programs/balwadis" component={Balwadis} />
+				<Route
+					exact
+					path="/programs/education-program"
+					component={EducationProgram}
+				/>
+				<Route
+					exact
+					path="/programs/children-camps"
+					component={ChildrenCamps}
+				/>
+				<Route
+					exact
+					path="/programs/ultimate-frisbee"
+					component={FrisbeeProgram}
+				/>
+				<Route exact path="/programs/" component={ProgramsHome} />
+			</Switch>
+		</MainContainer>
+	);
 };
 
 export default Programs;
--- frontend/src/Components/pages/Programs/Programs.styles.jsx
+++ frontend/src/Components/pages/Programs/Programs.styles.jsx
@@ -1,8 +1,47 @@
 import styled from "styled-components";
 import { colorObj } from "../../../Styles/GlobalStyles";
 
-
 export const MainContainer = styled.div`
-  background: #f8f7f7;
-  min-height: 100%;
+	background: #f8f7f7;
+	padding: 150px 0 100px 0;
+`;
+
+export const Heading = styled.h2`
+	font-size: 6.4rem;
+	font-family: "Roboto", sans-serif;
+	color: ${colorObj.dark};
+	font-weight: 700;
+`;
+
+export const Container = styled.div`
+	width: 80%;
+	margin: 0 auto;
+	text-align: center;
+	height: 100%;
+`;
+
+export const HeadingContainer = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+	height: 150px;
+`;
+
+export const BackBtn = styled.div`
+	position: absolute;
+	bottom: 10px;
+	color: ${colorObj.dark};
+	left: 10px;
+	font-weight: 900;
+	font-size: 1.5rem;
+	cursor: pointer;
+
+	&:before {
+		content: "\u00AB";
+		font-size: 2rem;
+		color: black;
+		font-weight: 900;
+		margin-right: 3px;
+	}
 `;
