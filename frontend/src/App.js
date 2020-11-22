fa617c14aa5e9f9bddce21d10fbc7f7a403d12b4
ianbautista <ianfbautista@gmail.com>
2020-11-22 11:37:24 -0800

    fixes routing for program components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Programs from './Components/pages/Programs/Programs.component';
 import About from './Components/pages/About';
