33e22006fb822bf179e3378492fe8436e852956b
Harry Henry Gebel <harry@gebel.tech>
2020-11-21 21:47:59 -0500

    Article javadoc comments

--- backend/src/main/java/com/opportunity/hack/vidyodaya/models/Article.java
+++ backend/src/main/java/com/opportunity/hack/vidyodaya/models/Article.java
@@ -2,21 +2,54 @@ package com.opportunity.hack.vidyodaya.models;
 
 import javax.persistence.*;
 
+/**
+ * Model for table Article - holds articles from website
+ */
 @Entity
 @Table(name = "articles")
 public class Article extends Auditable {
 
+  /**
+   * The database id of the article
+   */
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long articleId;
 
+  /**
+   * The article title
+   */
   @Column(nullable = false, unique = true)
   private String title;
 
+  /**
+   * The description of the article
+   */
   private String description;
+
+  /**
+   * The url of the article's thumbnail image
+   */
+  private String imageUrl;
+
+  /**
+   * The contents of the article as a stored pdf file
+   */
+  @Lob
+  private byte[] article;
 
   public Article() {}
 
+  /**
+   * Primary constructor
+   * @param articleId The database id of the article
+   * @param title The article title
+   * @param description The description of the article
+   * @param imageUrl The url of the article's thumbnail image
+   * @param article The contents of the article as a stored pdf file
+   */
+  public Article(long articleId, String title, String description, String imageUrl, byte[] article) {
+    this.articleId = articleId;
     this.title = title;
     this.description = description;
     this.imageUrl = imageUrl;
