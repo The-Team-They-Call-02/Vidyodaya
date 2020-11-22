ce770b69c60606d3d4a76f44862485f1b3ba93b3
Harry Henry Gebel <harry@gebel.tech>
2020-11-22 10:13:33 -0500

    Add camps/ endpoint

--- backend/src/main/java/com/opportunity/hack/vidyodaya/controllers/CampController.java
+++ backend/src/main/java/com/opportunity/hack/vidyodaya/controllers/CampController.java
@@ -1,8 +1,28 @@
 package com.opportunity.hack.vidyodaya.controllers;
 
+import com.opportunity.hack.vidyodaya.models.Camp;
+import com.opportunity.hack.vidyodaya.services.CampService;
+import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/camps")
-public class CampController {}
+public class CampController {
+
+  private final CampService campService;
+
+  public CampController(CampService campService) {
+    this.campService = campService;
+  }
+
+  @GetMapping(value = "", produces = "application/json")
+  public ResponseEntity<?> listAllCamps() {
+    List<Camp> camps = campService.findAll();
+
+    return new ResponseEntity<>(camps, HttpStatus.OK);
+  }
+}
