f23d9a7b1098d85a07977c988decbc223dc48375
Harry Henry Gebel <harry@gebel.tech>
2020-11-22 10:12:47 -0500

    add CampService.findall

--- backend/src/main/java/com/opportunity/hack/vidyodaya/services/CampService.java
+++ backend/src/main/java/com/opportunity/hack/vidyodaya/services/CampService.java
@@ -1,3 +1,12 @@
 package com.opportunity.hack.vidyodaya.services;
 
-public interface CampService {}
+import com.opportunity.hack.vidyodaya.models.Camp;
+import java.util.List;
+
+public interface CampService {
+  /**
+   * Return a list of al camps
+   * @return List of all Camp instances
+   */
+  List<Camp> findAll();
+}
--- backend/src/main/java/com/opportunity/hack/vidyodaya/services/CampServiceImplementation.java
+++ backend/src/main/java/com/opportunity/hack/vidyodaya/services/CampServiceImplementation.java
@@ -1,3 +1,32 @@
 package com.opportunity.hack.vidyodaya.services;
 
-public class CampServiceImplementation {}
+import com.opportunity.hack.vidyodaya.models.Camp;
+import com.opportunity.hack.vidyodaya.repository.CampRepository;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Service(value = "campService")
+public class CampServiceImplementation implements CampService {
+
+  private final CampRepository campRepository;
+
+  public CampServiceImplementation(CampRepository campRepository) {
+    this.campRepository = campRepository;
+  }
+
+  /**
+   * Return a list of al camps
+   *
+   * @return List of all Camp instances
+   */
+  @Override
+  public List<Camp> findAll() {
+    List<Camp> camps = new ArrayList<>();
+
+    campRepository.findAll().iterator().forEachRemaining(camps::add);
+    return camps;
+  }
+}
