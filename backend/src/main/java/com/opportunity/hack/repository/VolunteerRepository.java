bdb5218fcbb9a895d4ef033637e90c5808321ca8
Harry Henry Gebel <harry@gebel.tech>
2020-11-21 20:10:36 -0500

    Rename package to be less generic

--- backend/src/main/java/com/opportunity/hack/repository/VolunteerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.opportunity.hack.repository;
-
-import com.opportunity.hack.models.Volunteer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VolunteerRepository extends CrudRepository<Volunteer, Long> {}
---
+++ backend/src/main/java/com/opportunity/hack/vidyodaya/config/DataSourceConfig.java
@@ -0,0 +1,41 @@
+package com.opportunity.hack.vidyodaya.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataSourceConfig {
+
+  @Value("${local.run.db:h2}")
+  private String dbValue;
+
+  @Value("${spring.datasource.url:}")
+  private String dbURL;
+
+  @Bean
+  public DataSource dataSource() {
+    if (dbValue.equalsIgnoreCase("POSTGRESQL")) {
+      HikariConfig config = new HikariConfig();
+      config.setDriverClassName("org.postgresql.Driver");
+      config.setJdbcUrl(dbURL);
+      return new HikariDataSource(config);
+    } else {
+      String myURLString = "jdbc:h2:mem:testdb";
+      String myDriverClass = "org.h2.Driver";
+      String myDBUser = "sa";
+      String myDBPassword = "";
+      return DataSourceBuilder
+        .create()
+        .username(myDBUser)
+        .password(myDBPassword)
+        .url(myURLString)
+        .driverClassName(myDriverClass)
+        .build();
+    }
+  }
+}
