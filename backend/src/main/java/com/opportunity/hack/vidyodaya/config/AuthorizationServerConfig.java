b79ca860bccffab46575a49855ef571388e296d4
Harry Henry Gebel <harry@gebel.tech>
2020-11-23 00:32:38 -0500

    Bulk warning fixes

--- backend/src/main/java/com/opportunity/hack/vidyodaya/config/AuthorizationServerConfig.java
+++ backend/src/main/java/com/opportunity/hack/vidyodaya/config/AuthorizationServerConfig.java
@@ -1,6 +1,5 @@
 package com.opportunity.hack.vidyodaya.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -70,20 +69,23 @@ public class AuthorizationServerConfig
   /**
    * The token store is configured in Security Config. However, the authorization server manages it
    */
-  @Autowired
-  private TokenStore tokenStore;
+  private final TokenStore tokenStore;
 
   /**
    * The authentication server authenticates a user to that user user gets assigned an access token that is managed by the authorization server
    */
-  @Autowired
-  private AuthenticationManager authenticationManager;
+  private final AuthenticationManager authenticationManager;
 
   /**
    * The authorization server must encrypt the client secret so needs to know what password encoder is in use.
    */
-  @Autowired
-  private PasswordEncoder encoder;
+  private final PasswordEncoder encoder;
+
+  public AuthorizationServerConfig(TokenStore tokenStore, AuthenticationManager authenticationManager, PasswordEncoder encoder) {
+    this.tokenStore = tokenStore;
+    this.authenticationManager = authenticationManager;
+    this.encoder = encoder;
+  }
 
   /**
    * Method to configure the Client Details Service for our application. This is created and managed by Spring.
@@ -111,11 +113,9 @@ public class AuthorizationServerConfig
    *
    * @param endpoints The Authorization Server Endpoints Configurer is created and managed by Spring Boot Security.
    *                  We give the configurer some custom configuration and let it work!
-   * @throws Exception if the configuration fails
    */
   @Override
-  public void configure(AuthorizationServerEndpointsConfigurer endpoints)
-    throws Exception {
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
     endpoints
       .tokenStore(tokenStore)
       .authenticationManager(authenticationManager);
