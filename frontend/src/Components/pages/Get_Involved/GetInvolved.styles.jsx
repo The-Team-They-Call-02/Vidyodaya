304bd607fc3924c43d7e60a1249b6bb7723987a9
ianbautista <ianfbautista@gmail.com>
2020-11-24 02:09:30 -0800

    fixes view dashboard styling

--- frontend/src/Components/pages/Get_Involved/GetInvolved.styles.jsx
+++ frontend/src/Components/pages/Get_Involved/GetInvolved.styles.jsx
@@ -2,64 +2,65 @@ import styled from "styled-components";
 import { colorObj } from "../../../Styles/GlobalStyles";
 
 export const MainContainer = styled.div`
-  ${"" /* background: #f8f7f7; */}
-  padding: 150px 0 0 0;
+	${"" /* background: #f8f7f7; */}
+	padding: 150px 0 0 0;
 `;
 
 export const Heading = styled.h2`
-  font-size: 6.4rem;
-  font-family: "Roboto", sans-serif;
-  color: ${colorObj.dark};
-  font-weight: 700;
+	font-size: 6.4rem;
+	font-family: "Roboto", sans-serif;
+	color: ${colorObj.dark};
+	font-weight: 700;
 `;
 
 export const Container = styled.div`
-  width: 80%;
-  margin: 0 auto;
-  text-align: center;
-  padding-bottom: 50px;
-  min-height: calc(100vh - (40vh + 150px));
+	width: 80%;
+	margin: 0 auto;
+	text-align: center;
+	padding-bottom: 50px;
+	min-height: calc(100vh - (40vh + 150px));
 `;
 
 export const HeadingContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: relative;
-  height: 150px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	position: relative;
+	height: 150px;
 `;
 
 export const ViewDashboard = styled.button`
-  display: ${localStorage.getItem("token") ? "inline" : "none"};
-  border: 2px solid #00703a;
-  background: #e5e5e5;
-  position: absolute;
-  top: 50%;
-  right: 0;
-  transform: translateY(-50%);
-  padding: 10px;
-  border-radius: 10px;
-  text-transform: uppercase;
-  font-weight: 900;
-  color: #00703a;
-  width: 20rem;
-  height: 5rem;
+	display: ${localStorage.getItem("token") ? "inline" : "none"};
+	border: 2px solid #00703a;
+	background: rgba(255, 255, 255, 0.1);
+	position: absolute;
+	top: 50%;
+	right: 0;
+	transform: translateY(-50%);
+	padding: 10px;
+	border-radius: 10px;
+	text-transform: uppercase;
+	font-weight: 900;
+	color: #00703a;
+	width: 20rem;
+	height: 5rem;
+	cursor: pointer;
 `;
 
 export const BackBtn = styled.div`
-  position: absolute;
-  bottom: 10px;
-  color: ${colorObj.dark};
-  left: 10px;
-  font-weight: 900;
-  font-size: 1.5rem;
-  cursor: pointer;
+	position: absolute;
+	bottom: 10px;
+	color: ${colorObj.dark};
+	left: 10px;
+	font-weight: 900;
+	font-size: 1.5rem;
+	cursor: pointer;
 
-  &:before {
-    content: "\u00AB";
-    font-size: 2rem;
-    color: black;
-    font-weight: 900;
-    margin-right: 3px;
-  }
+	&:before {
+		content: "\u00AB";
+		font-size: 2rem;
+		color: black;
+		font-weight: 900;
+		margin-right: 3px;
+	}
 `;
